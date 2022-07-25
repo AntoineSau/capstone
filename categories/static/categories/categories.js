75e7b2308959254f3fe0fa4b76deaf12e3fa0b05
AntoineSau <69819028+AntoineSau@users.noreply.github.com>
2022-07-25 18:10:41 +0200

    Adding new files

---
+++ categories/static/categories/categories.js
@@ -0,0 +1,62 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    // hide off and onlien cotnent at the beginning
+    document.querySelector('#online_content').style.display = 'none';
+    document.querySelector('#offline_content').style.display = 'none';
+
+    // Use buttons to toggle between views
+    document.querySelector('#offline_button').addEventListener('click', offline_view);
+    document.querySelector('#online_button').addEventListener('click', online_view);
+
+});
+
+function online_view() {
+
+    // Show relevant content div
+    document.querySelector('#online_content').style.display = 'block';
+    document.querySelector('#offline_content').style.display = 'none';
+    
+    // Check LOG
+    console.log('online clicked');
+
+};
+
+function offline_view() {
+
+    // Show relevant content div
+    document.querySelector('#offline_content').style.display = 'block';
+    document.querySelector('#online_content').style.display = 'none';
+
+    // Check LOG
+    console.log('offline clicked');
+
+};
+
+function generate_letter() {
+    
+    // Test to retrieve list of checked checkboxes
+    var inputs = document.getElementsByTagName("input");
+    var checked = [];
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].type == "checkbox") {
+            if (inputs[i].checked) {
+                checked.push(inputs[i].value);
+            }
+        }
+    }
+    
+    document.getElementById('print_test').innerHTML = `${checked}`;
+
+    // Generate a random letter from selected lsit only
+    // OK let letters_available = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    let letters_available = checked
+    let random_letter = letters_available[Math.floor(Math.random() * letters_available.length)]
+    document.getElementById('letter_to_play').innerHTML = `=> ${random_letter}`;
+
+    
+
+    // Add it to DOM
+    console.log(random_letter)
+
+
+};
