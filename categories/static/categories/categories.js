9452873dc97f993a1d3caf39d2617fb465a33f7f
AntoineSau <69819028+AntoineSau@users.noreply.github.com>
2022-08-21 15:28:02 +0200

    Basic ON and OFF. Timers separated

--- categories/static/categories/categories.js
+++ categories/static/categories/categories.js
@@ -8,6 +8,15 @@ document.addEventListener('DOMContentLoaded', function() {
     const intro_block = document.getElementById('intro_block');
     const rules_content = document.querySelector('#rules_content');
     rules_content.style.display = 'none';
+    // Hide the whole Generate letter and offline forms to focus on relevant content
+    const form_offline_game = document.getElementById('form_offline_game');
+    form_offline_game.style.display = 'block';
+    const form_generate_letter = document.getElementById('form_generate_letter');
+    form_generate_letter.style.display = 'block';
+    const game_data = document.getElementById('game_data');
+    game_data.style.display = "none";
+    const game_data_off = document.getElementById('game_data_off');
+    game_data_off.style.display = "none";
 
     // Use buttons to toggle between views
     document.querySelector('#offline_button').addEventListener('click', offline_view);
@@ -19,6 +28,12 @@ document.addEventListener('DOMContentLoaded', function() {
     for (let i = 0; i < alphabet_js.length; i++) {    
         alphabet_generated.innerHTML += `<button id="${alphabet_js[i]}" type="button" onclick="switch_color('${alphabet_js[i]}')" class="btn btn-outline-success">${alphabet_js[i]}</button> `;
     }
+    // offline view
+    const alphabet_generated_off = document.getElementById('alphabet_off');
+    for (let i = 0; i < alphabet_js.length; i++) {    
+        alphabet_generated_off.innerHTML += `<button id="${alphabet_js[i]}_off" type="button" onclick="switch_color('${alphabet_js[i]}_off')" class="btn btn-outline-success btn-lg">${alphabet_js[i]}</button> `;
+    }
+
 
     // Populate list of categories
     var categories = ["First Name", "Song title", "Band", "Country", "Item in this room", 
@@ -65,6 +80,14 @@ function switch_color(this_button) {
         button_to_change.className = "btn btn-outline-success";
     }
 
+    // Adaptation for OFFline leetrs
+    else if (button_to_change.className == "btn btn-outline-success btn-lg") {
+        button_to_change.className = "btn btn-outline-dark btn-lg";
+    }
+    else if (button_to_change.className == "btn btn-outline-dark btn-lg") {
+        button_to_change.className = "btn btn-outline-success btn-lg";
+    }
+
     // let cat = document.getElementById(this_button).innerHTML;
     // Print Check 
     // console.log(`You want to change ${cat}`);
@@ -74,23 +97,28 @@ function online_view() {
 
     // Hide introduction block
     intro_block.style.display = 'none';
+    game_data.style.display = 'none';
 
     // Show relevant content div
     online_content.style.display = 'block';
     offline_content.style.display = 'none';
     rules_content.style.display = 'none';
-
+    form_generate_letter.style.display = 'block';
+    
+    
 };
 
 function offline_view() {
 
     // Hide introduction block
     intro_block.style.display = 'none';
+    game_data_off.style.display = 'none';
  
     // Show relevant content div
     offline_content.style.display = 'block';
     online_content.style.display = 'none';
     rules_content.style.display = 'none';
+    form_offline_game.style.display = 'block';
 
 };
 
@@ -106,13 +134,18 @@ function rules_view() {
 
 };
 
+// BACK UP for online game
 async function generate_letter() {
     
     // Clean up categories field if users restarts timer
+    game_data.style.display = "block";
     const print_test = document.getElementById('print_test');
-    print_test .innerHTML = "";
+    print_test.innerHTML = "";
     const print_categories = document.getElementById('print_categories')
     print_categories.innerHTML = "";
+    // Hide forms
+    form_generate_letter.style.display = 'none';
+    form_offline_game.style.display = 'none';
 
     // Retrieve list of checked checkboxes
     var all_buttons = document.getElementsByTagName("button");
@@ -132,11 +165,8 @@ async function generate_letter() {
     }
 
     // Retrieve timer
-    for (let i = 0; i < inputs.length; i++) {
-        if (inputs[i].type == "number") {
-            timer = inputs[i].value;
-        }
-    }
+    timer_on = document.getElementById('timer_on');
+    timer = timer_on.value;
 
     // Count the amount of checked boxes
     var amount_of_letters = selected_letters.length;
@@ -168,10 +198,6 @@ async function generate_letter() {
     // print_test.innerHTML = `<h3><i>PRINT CHECK: -> ${checked} with timer of "${timer}". 
     // LENGTH = ${checked_boxes}</i></h3>
     // CATEGORIES: ${categories_selected}`;
-
-    // Hide the whole Generate letter form to focus on relevant content
-    const form_generate_letter = document.getElementById('form_generate_letter');
-    form_generate_letter.style.display = 'none';
 
     // Print test length categories
     print_test.innerHTML = `Your <b>${categories_selected.length}</b> categories are:`;
@@ -237,8 +263,8 @@ async function generate_letter() {
     } 
 
     // Sound alert if user doesn´t look at screen
-    var alert_audio = new Audio('static/categories/Alert Categories project.mp3');
-    alert_audio.play();
+    //var alert_audio = new Audio('static/categories/Alert Categories project.mp3');
+    //alert_audio.play();
 
     // Tell user that time is up and add button to start again
     current_timer.innerHTML = `Time is up!`;
@@ -279,6 +305,143 @@ async function generate_letter() {
     // Add it to DOM
     console.log(random_letter);
 
+};
+
+async function offline_game() {
+    
+    game_data_off.style.display = 'block';
+
+    // Hide forms
+    form_generate_letter.style.display = 'none';
+    form_offline_game.style.display = 'none';
+
+    // Retrieve list of checked checkboxes
+    var all_buttons = document.getElementsByTagName("button");
+    var inputs = document.getElementsByTagName("input");
+    var selected_letters_off = [];
+    // Put timer to blue
+    var current_timer_off = document.getElementById('print_timer_off');
+    current_timer_off.style.color = "blue";
+    
+    // Retrieve letters selected
+    for (let i = 0; i < all_buttons.length; i++) {
+        if (all_buttons[i].className == "btn btn-outline-success btn-lg") {
+            selected_letters_off.push(all_buttons[i].innerHTML);
+        } 
+    }
+
+    // Retrieve timer
+    timer_off = document.getElementById('timer_off');
+    timer = timer_off.value;
+
+    // Count the amount of checked boxes
+    var amount_of_letters_off = selected_letters_off.length;
+
+    // Cancel if amount of letters selected below 3
+    if (amount_of_letters_off < 5) {
+        alert(`Please select at least 5 letters`);
+        form_offline_game.style.display = 'block';
+        return false;
+    }
+   
+    // Print check
+    // print_test.innerHTML = `<h3><i>PRINT CHECK: -> ${checked} with timer of "${timer}". 
+    // LENGTH = ${checked_boxes}</i></h3>
+    // CATEGORIES: ${categories_selected}`;
+
+    // Hide form:
+    form_offline_game.style.display = 'none';
+
+    // Lock Button in order to avoid having 2 timers running at the same time
+    const offline_generator = document.getElementById("offline_generator");
+    offline_generator.innerHTML = 'Wait until the end of the timer';
+    offline_generator.disabled = true;
+
+    // Generate a random letter from selected list only
+    var random_letter_off = selected_letters_off[Math.floor(Math.random() * selected_letters_off.length)];
+    var letter_to_play_off = document.getElementById('letter_to_play_off');
+    letter_to_play_off.style.color = "blue";
+    letter_to_play_off.innerHTML = `${random_letter_off}`;
+
+    // "Freeze whole menu when timer is running"
+    // Get all links in menu for further use
+    const all_menu_links = document.getElementsByClassName("nav-link");
+
+    // Loop over all of them and disable them
+    const all_menu_links_length = all_menu_links.length;
+    for (let i = 0; i < all_menu_links_length; i++) {
+        if (i == 0) {
+            all_menu_links[i].className = "nav-link disabled";
+            all_menu_links[i].innerHTML = `<i>Timer must be finished or closed to switch view</i>`;
+        } else {
+            all_menu_links[i].className = "nav-link disabled";
+            all_menu_links[i].innerHTML = "";
+        }
+  
+    }
+    
+    // Display the total amount of seconds the user wants to play
+    for (let i = timer; i > 0; i--) {
+        console.log(`Timer is ${timer}`);
+
+        // Give the possibility to the users to stop the timer while it´s running
+        const stop_timer_off = document.getElementById('stop_timer_off');
+        stop_timer_off.innerHTML = `<button id="button_stop_timer_off" class="btn btn-warning">Stop timer</button>`;
+        document.querySelector('#button_stop_timer_off').addEventListener('click', function () {i = 0});
+
+        current_timer_off.innerHTML = `${timer}`;
+        // call Sleep function for delay
+        await sleep(1000);
+        
+        timer--;
+    }
+
+    // Loop over menu to reactivate each part
+    const menu_items = ['Play Online','Play Offline', 'How to Play'];
+    for (let i = 0; i < all_menu_links_length; i++) {
+        
+            all_menu_links[i].className = "nav-link";
+            all_menu_links[i].innerHTML = `${menu_items[i]}`;
+
+    } 
+
+    // Sound alert if user doesn´t look at screen
+    //var alert_audio = new Audio('static/categories/Alert Categories project.mp3');
+    //alert_audio.play();
+
+    // Tell user that time is up and add button to start again
+    current_timer_off.innerHTML = `Time is up!`;
+    // Hide forms
+    form_generate_letter.style.display = 'none';
+    form_offline_game.style.display = 'none';
+
+    // Hide stop timer button
+    stop_timer_off.innerHTML = "";
+
+    // Animation to show easily that time is up (background to black and texts to white, and back)
+    document.body.style.backgroundColor = "black";
+    letter_to_play_off.style.color = "white";
+    current_timer_off.style.color = "white";
+    await sleep(1000);
+    document.body.style.backgroundColor = "white";
+    letter_to_play_off.style.color = "grey";
+    current_timer_off.style.color = "grey";
+
+    current_timer_off.innerHTML += `<p><button onclick="offline_game()" class="btn btn-info mb-2 btn-lg">Play again</button>
+    <button onclick="postgame_view_off()" class="btn btn-light mb-2 btn-lg">Change data</button>`;
+
+    // Put generate timer button back
+    offline_generator.innerHTML = 'Generate a random letter and set timer';
+    offline_generator.disabled = false;
+    
+    // Test sleep function
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    // Add it to DOM
+    console.log(random_letter);
+
     // Give back possibility to switch mode
     change_to_online_mode.innerText = 'Play online';
     change_to_online_mode.href = 'javascript:online_view();';
@@ -295,5 +458,16 @@ function postgame_view () {
 
     // Show again the 'Generate letter' so the users can modify data
     form_generate_letter.style.display = 'block';
+
+}
+
+function postgame_view_off () {
+    // Clean up categories field if users want to change variables
+    letter_to_play_off.innerHTML = "";
+    var current_timer_off = document.getElementById('print_timer_off');
+    current_timer_off.innerHTML = "";
+
+    // Show again the 'Generate letter' so the users can modify data
+    form_offline_game.style.display = 'block';
 
 }
